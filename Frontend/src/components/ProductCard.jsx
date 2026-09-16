1594d78a1e18d166aa7a47804d333118021abce4
Jatinonly <jatinpilpia123@gmail.com>
2026-09-16 17:04:31 +0530

    first commit

--- Frontend/src/components/ProductCard.jsx
+++ Frontend/src/components/ProductCard.jsx
@@ -1,9 +1,6 @@
-
 import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 import useAppStore from "../store/appStore";
-
-
 
 const ProductCard = ({ product }) => {
   const navigate = useNavigate();
@@ -13,14 +10,13 @@ const ProductCard = ({ product }) => {
   const removeFromCart = useAppStore((state) => state.removeFromCart);
   const cartItems = useAppStore((state) => state.cartItems);
 
-
   return (
     product && (
       <div className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
         <div className="group cursor-pointer flex items-center justify-center px-2">
           <img
             className="group-hover:scale-105 transition max-w-26 md:max-w-36"
-            src={product.image}
+            src={product.image[0]}
             alt={product.name}
           />
         </div>
@@ -33,7 +29,7 @@ const ProductCard = ({ product }) => {
             {Array(5)
               .fill("")
               .map((_, i) =>
-                product.rating > i ? (
+                4 > i ? (
                   <svg
                     key={i}
                     width="14"
@@ -49,6 +45,7 @@ const ProductCard = ({ product }) => {
                   </svg>
                 ) : (
                   <svg
+                    key={i}
                     width="14"
                     height="13"
                     viewBox="0 0 18 17"
@@ -58,21 +55,23 @@ const ProductCard = ({ product }) => {
                     <path
                       d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
                       fill="#615fff"
-                      fill-opacity="0.35"
+                      fillOpacity="0.35"
                     />
                   </svg>
                 ),
               )}
-            <p>({product.rating})</p>
+            <p>(4)</p>
           </div>
           <div className="flex items-end justify-between mt-3">
             <p className="md:text-xl text-base font-medium text-primary">
-              {currency}{product.offerPrice}{" "}
+              {currency}
+              {product.offerPrice}{" "}
               <span className="text-gray-500/60 md:text-sm text-xs line-through">
-                {currency}{product.price}
+                {currency}
+                {product.price}
               </span>
             </p>
-            <div onClick={(e)=>e.stopPropagation()} className="text-primary">
+            <div onClick={(e) => e.stopPropagation()} className="text-primary">
               {!cartItems[product._id] ? (
                 <button
                   className="flex items-center justify-center cursor-pointer gap-1 bg-primary/10 border border-primary/40 md:w-[80px] w-[64px] h-[34px] rounded text-primary font-medium"
@@ -84,14 +83,20 @@ const ProductCard = ({ product }) => {
               ) : (
                 <div className="flex items-center justify-center gap-2 md:w-20 w-16 h-[34px] bg-primary/25 rounded select-none">
                   <button
-                    onClick={() => {removeFromCart(product._id)}}
+                    onClick={() => {
+                      removeFromCart(product._id);
+                    }}
                     className="cursor-pointer text-md px-2 h-full"
                   >
                     -
                   </button>
-                  <span className="w-5 text-center">{cartItems[product._id]}</span>
+                  <span className="w-5 text-center">
+                    {cartItems[product._id]}
+                  </span>
                   <button
-                    onClick={() => {addToCart(product._id)}}
+                    onClick={() => {
+                      addToCart(product._id);
+                    }}
                     className="cursor-pointer text-md px-2 h-full"
                   >
                     +
