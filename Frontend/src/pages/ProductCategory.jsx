a371d31b51e67ac28040bb9508e4714006b5b1e0
Jatinonly <jatinpilpia123@gmail.com>
2026-09-19 15:29:09 +0530

    18 sep updates

--- Frontend/src/pages/ProductCategory.jsx
+++ Frontend/src/pages/ProductCategory.jsx
@@ -14,26 +14,30 @@ const ProductCategory = () => {
   );
 
   return (
-    <div className= "mt-16" >
+    <div className="mt-16">
       {searchCategory && (
         <div className="flex flex-col items-end w-max">
           <p className=" text-2xl font-medium">
             {searchCategory.text.toUpperCase()}
           </p>
-          <div className="w-16 h-0.5 bg-primary rounded-full"><ProductCard /></div>
+          <div className="w-16 h-0.5 bg-primary rounded-full">
+            <ProductCard />
+          </div>
         </div>
       )}
       {filteredProducts.length > 0 ? (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6  mt-6">{filteredProducts.map((product) => (
-          <ProductCard product={product} key={product._id} />
-        )) }</div>
-      ):
-      (
-        <div className="flex items-center justify-center h-[60vh]">
-          <p className="text-2xl font-medium text-primary">No Products found in this Category</p>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6  mt-6">
+          {filteredProducts.map((product) => (
+            <ProductCard product={product} key={product._id} />
+          ))}
         </div>
-      )
-      }
+      ) : (
+        <div className="flex items-center justify-center h-[60vh]">
+          <p className="text-2xl font-medium text-primary">
+            No Products found in this Category
+          </p>
+        </div>
+      )}
     </div>
   );
 };
--- Frontend/src/store/appStore.jsx
+++ Frontend/src/store/appStore.jsx
@@ -4,62 +4,86 @@ import { toast } from "react-hot-toast";
 
 const currency = import.meta.env.VITE_CURRENCY;
 
-const useAppStore = create((set) => ({
-  currency,
+const useAppStore = create((set, get) => ({
+    currency,
 
-  user: null,
-  isSeller: false,
-  showUserLogin: false,
-  products: [],
-  cartItems: {},
-  searchQuery: "",
+    user: null,
+    isSeller: false,
+    showUserLogin: false,
+    products: [],
+    cartItems: {},
+    searchQuery: "",
 
-  setUser: (user) => set({ user }),
-  setIsSeller: (isSeller) => set({ isSeller }),
-  setShowUserLogin: (showUserLogin) => set({ showUserLogin }),
-  setProducts: (products) => set({ products }),
-  setCartItems: (cartItems) => set({ cartItems }),
-  setSearchQuery: (searchQuery) => set({ searchQuery }),
-  addToCart: (itemId) => {
-    set((state) => {
-      let cartData = structuredClone(state.cartItems);
+    setUser: (user) => set({ user }),
+    setIsSeller: (isSeller) => set({ isSeller }),
+    setShowUserLogin: (showUserLogin) => set({ showUserLogin }),
+    setProducts: (products) => set({ products }),
+    setCartItems: (cartItems) => set({ cartItems }),
+    setSearchQuery: (searchQuery) => set({ searchQuery }),
 
-      if (cartData[itemId]) {
-        cartData[itemId] += 1;
-      } else {
-        cartData[itemId] = 1;
-      }
+    addToCart: (itemId) => {
+        set((state) => {
+            let cartData = structuredClone(state.cartItems);
 
-      return { cartItems: cartData };
-    });
-  toast.success("Added to Cart");
-  },
-  updateCartItem: (itemId,quantity) => {
-    set((state) => {
-      let cartData = structuredClone(state.cartItems);
-      cartData[itemId] = quantity;
-      setCartItems(cartData);
-      toast.success("Cart updated");
-      return { cartItems: cartData };
-    });
-  },
-  removeFromCart: (itemId) => {
-    set((state) => {
-      let cartData = structuredClone(state.cartItems);
-      if (cartData[itemId]) {
-        cartData[itemId] -= 1;
-        if (cartData[itemId]===0) {
-        delete cartData[itemId];
-      } 
-      } 
-      return { cartItems: cartData };
-    });
-    toast.success("Removed from Cart");
-  },
+            if (cartData[itemId]) {
+                cartData[itemId] += 1;
+            } else {
+                cartData[itemId] = 1;
+            }
 
-  fetchProducts: async () => {
-    set({ products: dummyProducts });
-  },
+            return { cartItems: cartData };
+        });
+        toast.success("Added to Cart");
+    },
+
+    updateCartItem: (itemId, quantity) => {
+        set((state) => {
+            let cartData = structuredClone(state.cartItems);
+            cartData[itemId] = quantity;
+            get().setCartItems(cartData);
+            return { cartItems: cartData };
+        });
+        toast.success("Cart updated");
+    },
+
+    removeFromCart: (itemId) => {
+        set((state) => {
+            let cartData = structuredClone(state.cartItems);
+            if (cartData[itemId]) {
+                cartData[itemId] -= 1;
+                if (cartData[itemId] === 0) {
+                    delete cartData[itemId];
+                }
+            }
+            return { cartItems: cartData };
+        });
+        toast.success("Removed from Cart");
+    },
+
+    fetchProducts: async () => {
+        set({ products: dummyProducts });
+    },
+
+    getCartCount: () => {
+        const { cartItems } = get();
+        let totalCount = 0;
+        for (const item in cartItems) {
+            totalCount += cartItems[item];
+        }
+        return totalCount;
+    },
+
+    getCartAmount: () => {
+        const { cartItems, products } = get();
+        let totalAmount = 0;
+        for (const items in cartItems) {
+            let itemInfo = products.find((product) => product._id === items);
+            if (cartItems[items] > 0) {
+                totalAmount += itemInfo.offerPrice * cartItems[items];
+            }
+        }
+        return Math.floor(totalAmount * 100) / 100;
+    },
 }));
 
 export default useAppStore;
