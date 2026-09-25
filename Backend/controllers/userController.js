fc145261e9886f4a29d27837186d01d2d15930ed
Jatinonly <jatinpilpia123@gmail.com>
2026-09-26 00:50:18 +0530

    25 sep updates

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -34,11 +34,90 @@ export const register = async (req, res) => {
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
-        res.json({
+        return res.json({
             success: true,
             user: { email: user.email, name: user.name },
         });
     } catch (error) {
         res.json({ success: false, message: error.message });
+    }
+};
+
+//login
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.json({
+                success: false,
+                message: "Email & password required",
+            });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.json({
+                success: false,
+                message: "No user exist!! Invalid email or password",
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.json({
+                success: false,
+                message: "Invalid email or Password",
+            });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "7D",
+        });
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+
+        return res.json({
+            success: true,
+            user: { email: user.email, name: user.name },
+        });
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+    }
+};
+
+//check auth
+
+export const isAuth = async (req, res) => {
+    try {
+        const { userId } = req.body;
+        const user = await User.findById(userId).select("-password -__v");
+        return res.json({ success: true, user });
+    } catch (error) {
+        console.log(error.message);
+        return res.json({ success: false, message: error.message });
+    }
+};
+
+//logout user
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        });
+
+        return res.json({ success: true, mesaage: "logged out" });
+    } catch (error) {
+        console.log(error.message);
+        return res.json({ success: false, mesaage: error.message });
     }
 };
