ddc71620fd9a0b03e1a762653032ea5075c44ec7
Jatinonly <jatinpilpia123@gmail.com>
2026-09-26 19:23:57 +0530

    Renamed folder and updated db.js

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () =>
       console.log("Database Connected"),
     );
-    await mongoose.connect(`${process.env.MONGO_URI}/urbancart`);
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
     console.log(error.message);
     process.exit(1);
